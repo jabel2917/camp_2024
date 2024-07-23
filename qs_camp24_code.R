4e4d3e29f48a118654fd2aa2426c8645e631ae6b
quinnlanconnorsmith <quinnlanconnorsmith@gmail.com>
2024-07-23 08:48:40 -0500

    Add preyfish

--- qs_camp24_code.R
+++ qs_camp24_code.R
@@ -27,7 +27,7 @@ bmi_sum_percentage <- bmi_sum %>%
 #Plot families by basin by number 
 bmi_sbp <- ggplot(bmi_sum, aes(x = basin, y = total_num, fill = family)) +
   geom_bar(stat = "identity") +
-  labs(title = "Percentage of Each Family in Each Basin",
+  labs(title = "Number of Families in Each Basin",
        x = "Basin",
        y = "Percentage") +
   theme_minimal()
@@ -36,12 +36,41 @@ bmi_sbp
 #Plot families by basin by percentage 
 bmi_sbp_percentage <- ggplot(bmi_sum_percentage, aes(x = basin, y = percentage, fill = family)) +
   geom_bar(stat = "identity") +
-  labs(title = "Percentage of Each Family in Each Basin",
+  labs(title = "Percentage of Families in Each Basin",
        x = "Basin",
        y = "Percentage") +
   theme_minimal()
 bmi_sbp_percentage
 
+####Preyfish 2024####
 
+#Note - removed perch and minnowtraps from these data to avoid 0 inflation 
+preyfish_2024 <- read_csv("preyfish_2024.csv")
+View(preyfish_2024)
 
+#Cleaning, omit NAs 
+preyfish_2024 <- na.omit(preyfish_2024)
+
+preyfish_bp <- ggplot(preyfish_2024, aes(x = basin, y = CPUE)) +
+  geom_boxplot() +                       # Add boxplot
+  geom_jitter(width = 0.2, alpha = 0.6) + # Add individual points with jitter for better visibility
+  labs(title = "CPUE per Basin - All fish",
+       x = "Basin",
+       y = "CPUE") +
+  theme_minimal()
+preyfish_bp
+
+#Omit LMB for giggles 
+blg_preyfish <- preyfish_2024 %>%
+  filter(species != 'LMB')
+
+#plot CPUE without LMB 
+blg_bp <- ggplot(blg_preyfish, aes(x = basin, y = CPUE)) +
+  geom_boxplot() +                       
+  geom_jitter(width = 0.1, alpha = 0.6, shape=15) + 
+  labs(title = "CPUE per Basin - BLG",
+       x = "Basin",
+       y = "CPUE") +
+  theme_minimal()
+blg_bp
 
